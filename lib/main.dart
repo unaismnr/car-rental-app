2e928271f30b63f52f828cf4589c3ab3c9652aef
unais mnr <mnrunais@gmail.com>
2024-07-18 16:30:25 +0530

    update

---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:car_rental_app/firebase_options.dart';
+import 'package:car_rental_app/onboarding_page.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(
+            seedColor: Colors.deepPurple,
+          ),
+          useMaterial3: true,
+        ),
+        home: const OnboardingPage());
+  }
+}
